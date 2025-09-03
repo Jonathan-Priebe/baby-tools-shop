0ad8bf9548230d49ceb2456cfeb752e4fbc3c074
Jonathan Priebe <jonathan.priebe@gmx.de>
2025-09-03 12:06:58 +0000

    Update: Change README add an example.env and changed settings.py

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY_Django")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "[::1]", os.getenv("IP_ADDR")]
+ALLOWED_HOSTS = [os.getenv("IP_ADDR")]
 
 # Application definition
 
